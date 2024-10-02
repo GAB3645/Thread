be70c896546d6bcc647ac7a40f5ebf64bcdeebfd
Gabriel <gabrielsiano2006@gmail.com>
2024-10-02 09:14:41 +0200

    third commit

--- src/main/java/it/itismeucci/Main.java
+++ src/main/java/it/itismeucci/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 
         Cestino c1 = new Cestino(0);
-        Deposito d1 = new Deposito(c1, 10000);
-        Deposito d2 = new Deposito(c1, 10000);
+        Deposito d1 = new Deposito(c1, 1000);
+        Deposito d2 = new Deposito(c1, 1000);
         d1.start();
         d2.start();
         d1.join();
